1bc65da82c30ba2003315944d1fea7575a84956d
Simon Schmidt <simon.schmidt@mabisile.de>
2021-09-11 15:31:35 +0200

    Corrected Maven Config

--- Core/src/main/java/basic/FunctionIdentifier.kt
+++ Core/src/main/java/basic/FunctionIdentifier.kt
@@ -2,4 +2,7 @@ package basic
 
 data class FunctionIdentifier<T>(val type: Class<T>, val number: Int)
 
-data class FunctionSignature(val valueT:Class<*>,val gradientT: Class<*>, val hessianT: Class<*>)
+data class FunctionSignature(val valueT: Class<*>, val gradientT: Class<*>, val hessianT: Class<*>)
+
+
+//HOW TO COMPILE KOTLIN FIRST IN MAVEN?
